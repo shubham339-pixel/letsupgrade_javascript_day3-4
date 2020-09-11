25fd4cf37c851417c965ceca62e4d194f167e2ed
shubham339-pixel <70780441+shubham339-pixel@users.noreply.github.com>
2020-09-11 20:24:18 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -1,8 +1,17 @@
-function copy(){
-    const text=document.getElementsByClassName("input");
+function change(){
+    const ele=document.getElementById("image");
+    const newImage="https://images.carandbike.com/bike-images/large/kawasaki/ninja-zx-10r/kawasaki-ninja-zx-10r.jpg?v=15";
+    ele.src=newImage
+}
 
- const text2=document.getElementsByClassName("input_2");
- text2[0].value=text[0].value;
- 
-    
+function change_again(){
+    const eles=document.getElementById("image");
+    const again="https://images.carandbike.com/bike-images/large/kawasaki/ninja-zx-14r/kawasaki-ninja-zx-14r.webp?v=6";
+    eles.src=again;
+}
+
+function reload() {
+const elem=document.getElementById("image");
+const reload_image="https://images.carandbike.com/bike-images/large/kawasaki/ninja-h2r/kawasaki-ninja-h2r.webp?v=4" 
+elem.src=reload_image;
 }
